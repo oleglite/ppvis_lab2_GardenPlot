30db817ee2ccd264bb0db3b886990ea5ec6ab665
Oleg Beloglazov <beloglazov.oleg@gmail.com>
2013-01-12 14:28:05 +0300

    first commit

---
+++ src/by/bsuir/ppvis/gardenplot/MainWindow.java
@@ -0,0 +1,19 @@
+package by.bsuir.ppvis.gardenplot;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Oleg Beloglazov
+ *
+ */
+public class MainWindow extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
